bf19f33d18e769084c086cbe3cb4d695e32d2e81
goshaaaa777 <isaiasddx322z@gmail.com>
2023-10-29 15:36:09 -0500

    examen cl2 isaias

--- src/test/java/pe/edu/cibertec/CL2IsaiasCapistrano/controller/administracion/FarmacoController.java
+++ src/test/java/pe/edu/cibertec/CL2IsaiasCapistrano/controller/administracion/FarmacoController.java
@@ -1,11 +1,11 @@
 package pe.edu.cibertec.CL2IsaiasCapistrano.controller.administracion;
 
-import pe.edu.cibertec.CL2IsaiasCapistrano.service.FarmacoService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.cibertec.CL2IsaiasCapistrano.service.FarmacoService;
 
 @AllArgsConstructor
 @Controller
--- src/test/java/pe/edu/cibertec/CL2IsaiasCapistrano/repository/FarmacoRepository.java
+++ src/test/java/pe/edu/cibertec/CL2IsaiasCapistrano/repository/FarmacoRepository.java
@@ -1,8 +1,8 @@
 package pe.edu.cibertec.CL2IsaiasCapistrano.repository;
 
-import pe.edu.cibertec.CL2IsaiasCapistrano.model.bd.Farmaco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pe.edu.cibertec.CL2IsaiasCapistrano.model.bd.Farmaco;
 
 @Repository
 public interface FarmacoRepository extends JpaRepository <Farmaco,
